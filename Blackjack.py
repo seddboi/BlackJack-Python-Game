9ec8394461bf9f461bd9ec98fa96120c69cb8af9
Gian Zamora <gz2016@att.net>
2023-08-06 19:05:32 -0700

    fully functional game... updateing features

--- Blackjack.py
+++ Blackjack.py
@@ -14,34 +14,96 @@ class Game:
 	
 	def game_start(self):
 		dealer1.shuffle_deck()
-		
-		# ****************************
-		# REduce 19-31 and make deal_card dictribute the card, remove from the deck, and append to user/dealers hand
-		# have it spit out a printable line describing first card, second card, etc (use class ids!)
-		user_card1 = dealer1.deal_card()
-		dealer_card1 = dealer1.deal_card()
-		user_card2 = dealer1.deal_card()
-		dealer_card2 = dealer1.deal_card()
 
-		game1.users_hand.append(user_card1)
-		print('Your first card: ' + card_class.format_card(user_card1))
-		game1.dealers_hand.append(dealer_card1)
-		print('Dealers first card: ' + card_class.format_card(dealer_card1))
-		game1.users_hand.append(user_card2)
-		print('Your second card: ' + card_class.format_card(user_card2))
-		game1.dealers_hand.append(dealer_card2)
+		dealer1.deal_card('user')
+		print('Your first card: ' + card_class.format_card(game1.users_hand[-1]))
+		dealer1.deal_card('dealer')
+		print('Dealers first card: ' + card_class.format_card(game1.dealers_hand[-1]))
+		dealer1.deal_card('user')
+		print('Your second card: ' + card_class.format_card(game1.users_hand[-1]))
+		dealer1.deal_card('dealer')
 
 		print('---------------------------------------')
 		print("Dealer's hand: " + dealer1.format_hand(game1.dealers_hand).split(' ')[0] + ' [hidden]')
 		print('Your hand: ' + dealer1.format_hand(game1.users_hand))
 
+		user_score = sum([x['value'] for x in game1.users_hand])
+		dealer_score = sum([x['value'] for x in game1.dealers_hand])
+
+		while True:
+			print('\n')
+			
+			choice = input('How would you like to continue? [enter "hit" to receive another card, or "stay" to end your turn]: ').lower()
+			if choice == 'hit':
+				dealer1.deal_card('user')
+				user_score = sum([x['value'] for x in game1.users_hand])
+				print('You were dealt a: ' + card_class.format_card(game1.users_hand[-1]))
+				print('Your new hand is: ' + dealer1.format_hand(game1.users_hand))
+				print('\n')
+			elif choice == 'stay':
+				print('You ended your turn with a score of ' + str(user_score))
+				print('\n')
+				break
+			else:
+				print('Invalid entry. Please try again.')
+				continue
+
+			if user_score > 21:
+				print('You ended with a score of ' + str(user_score))
+				print('BUST! You lose! :(')
+				break
+			elif user_score == 21:
+				print('BLACKJACK! You received a perfect score! You win!')
+				break
+		
+		while dealer_score < 17:
+			dealer1.deal_card('dealer')
+			user_score = sum([x['value'] for x in game1.users_hand])
+			dealer_score = sum([x['value'] for x in game1.dealers_hand])
+			if (user_score <= 21):
+				print('Dealer collects a: ' + card_class.format_card(game1.dealers_hand[-1]))
+				if dealer_score > 17:
+					print('Dealer reveals hand: ' + dealer1.format_hand(game1.dealers_hand))
+			else:
+				break
+		
+		if (user_score > 21):
+			return
+		else:
+			if dealer_score > 21:
+				print('You ended with a score of ' + str(user_score))
+				print('Dealer ended with a score of ' + str(dealer_score))
+				print('\n')
+				print('YOU WIN! :)')
+				print('\n')
+			elif( user_score > dealer_score):
+				print('You hand scores: ' + str(user_score))
+				print("The Dealer's hand scores: " + str(dealer_score))
+				print('\n')
+				print('YOU WIN! :)')
+				print('\n')
+			elif user_score < dealer_score:
+				print('You hand scores: ' + str(user_score))
+				print("The Dealer's hand scores: " + str(dealer_score))
+				print('\n')
+				print('You lose :(')
+				print('\n')
+			else:
+				print('You ended with a score of ' + str(user_score))
+				print('Dealer ended with a score of: ' + str(dealer_score))
+				print("It's a tie!")
+		
 class Dealer:	
 	def shuffle_deck(self):
 		random.shuffle(deck)
 
-	def deal_card(self):
+	def deal_card(self, is_user):
 		selected_card = deck[0]
 		deck.remove(selected_card)
+		if (is_user == 'user'):
+			game1.users_hand.append(selected_card)
+		else:
+			game1.dealers_hand.append(selected_card)
 		return selected_card
 	
 	def format_hand(self, current_hand):
