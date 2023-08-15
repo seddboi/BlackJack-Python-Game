3ff5890a09121bf97b8cf695309595b316dacf37
Gian Zamora <gz2016@att.net>
2023-08-15 15:06:18 -0700

    fixed replayability errors... next update will make game more realistic by allowing plays until deck is finished

--- Blackjack.py
+++ Blackjack.py
@@ -1,6 +1,33 @@
-from Classes.Game import *
-from Classes.Dealer import *
+import random
+
+from Classes.GameDeck import *
 from Classes.Card import *
+
+users_hand = []
+dealers_hand = []
+game_deck = deck
+
+
+def play_again_prompt():
+    while True:
+        play_again_option = input('Play again? ("yes" or "no")').lower()
+        if play_again_option == "yes":
+            users_hand.clear()
+            dealers_hand.clear()
+            global game_deck
+            game_deck = [
+                {"card": c, "suit": s, "value": v}
+                for [c, v] in zip(cards, values)
+                for s in suits
+            ]
+            play_game()
+            break
+        elif play_again_option == "no":
+            print("Goodbye :)")
+            exit()
+        else:
+            print("Invalid entry. Please try again.")
+            continue
 
 
 def introduction():
@@ -31,24 +58,23 @@ def introduction():
 
 
 def play_game():
-    d1.shuffle_deck()
+    random.shuffle(game_deck)
 
-    d1.deal_card("user")
-    print("Your first card: " + c1.format_card(g1.users_hand[-1]))
-    d1.deal_card("dealer")
-    print("Dealers first card: " + c1.format_card(g1.dealers_hand[-1]))
-    d1.deal_card("user")
-    print("Your second card: " + c1.format_card(g1.users_hand[-1]))
-    d1.deal_card("dealer")
+    print("\n")
+    deal_card("user")
+    print("Your first card: " + Card().format_card(users_hand[-1]))
+    deal_card("dealer")
+    print("Dealers first card: " + Card().format_card(dealers_hand[-1]))
+    deal_card("user")
+    print("Your second card: " + Card().format_card(users_hand[-1]))
+    deal_card("dealer")
 
     print("---------------------------------------")
-    print(
-        "Dealer's hand: " + d1.format_hand(g1.dealers_hand).split(" ")[0] + " [hidden]"
-    )
-    print("Your hand: " + d1.format_hand(g1.users_hand))
+    print("Dealer's hand: " + format_hand(dealers_hand).split(" ")[0] + " [hidden]")
+    print("Your hand: " + format_hand(users_hand))
 
-    user_score = sum([x["value"] for x in g1.users_hand])
-    dealer_score = sum([x["value"] for x in g1.dealers_hand])
+    user_score = sum([x["value"] for x in users_hand])
+    dealer_score = sum([x["value"] for x in dealers_hand])
 
     while True:
         print("\n")
@@ -57,10 +83,10 @@ def play_game():
             'How would you like to continue? [enter "hit" or "stay"]:'
         ).lower()
         if choice == "hit":
-            d1.deal_card("user")
-            user_score = sum([x["value"] for x in g1.users_hand])
-            print("You were dealt a: " + c1.format_card(g1.users_hand[-1]))
-            print("Your new hand is: " + d1.format_hand(g1.users_hand))
+            deal_card("user")
+            user_score = sum([x["value"] for x in users_hand])
+            print("You were dealt a: " + Card().format_card(users_hand[-1]))
+            print("Your new hand is: " + format_hand(users_hand))
             print("\n")
         elif choice == "stay":
             print("You ended your turn with a score of " + str(user_score))
@@ -73,19 +99,23 @@ def play_game():
         if user_score > 21:
             print("You ended with a score of " + str(user_score))
             print("BUST! You lose! :(")
+            print("\n")
+            play_again_prompt()
             break
         elif user_score == 21:
             print("BLACKJACK! You received a perfect score! You win!")
+            print("\n")
+            play_again_prompt()
             break
 
     while dealer_score < 17:
-        d1.deal_card("dealer")
-        user_score = sum([x["value"] for x in g1.users_hand])
-        dealer_score = sum([x["value"] for x in g1.dealers_hand])
+        deal_card("dealer")
+        user_score = sum([x["value"] for x in users_hand])
+        dealer_score = sum([x["value"] for x in dealers_hand])
         if user_score <= 21:
-            print("Dealer collects a: " + c1.format_card(g1.dealers_hand[-1]))
+            print("Dealer collects a: " + Card().format_card(dealers_hand[-1]))
             if dealer_score > 17:
-                print("Dealer reveals hand: " + d1.format_hand(g1.dealers_hand))
+                print("Dealer reveals hand: " + format_hand(dealers_hand))
         else:
             break
 
@@ -114,10 +144,27 @@ def play_game():
             print("You ended with a score of " + str(user_score))
             print("Dealer ended with a score of: " + str(dealer_score))
             print("It's a tie!")
+    play_again_prompt()
 
 
-g1 = Game()
-d1 = Dealer()
-c1 = Card()
+def deal_card(is_user):
+    selected_card = deck[0]
+    deck.remove(selected_card)
+    if is_user == "user":
+        users_hand.append(selected_card)
+    else:
+        dealers_hand.append(selected_card)
+    return selected_card
+
+
+def format_hand(current_hand):
+    user_hand_string = ""
+    for card in current_hand:
+        if card == current_hand[-1]:
+            user_hand_string += Card().format_card(card)
+        else:
+            user_hand_string += Card().format_card(card) + ", "
+    return user_hand_string
+
 
 introduction()
--- Classes/GameDeck.py
+++ Classes/GameDeck.py
@@ -5,10 +5,3 @@ values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 deck = [
     {"card": c, "suit": s, "value": v} for [c, v] in zip(cards, values) for s in suits
 ]
-
-
-class Game:
-    def __init__(self):
-        self.dealers_hand = []
-        self.users_hand = []
-        self.game_deck = deck
